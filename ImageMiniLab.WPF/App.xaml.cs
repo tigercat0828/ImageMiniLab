3d3ec186cdc2e33952df2dca7f8d69578ec2f7f4
Tigercat <tigercat0828@gmail.com>
2025-08-08 09:40:11 +0800

    Initial commit

---
+++ ImageMiniLab.WPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ImageMiniLab.WPF.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CodeDom;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ImageMiniLab.WPF {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private readonly IHost _host;
+
+        public App() {
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddSingleton<MainWindow>();
+            _host = builder.Build();
+            
+        }
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            var Window = _host.Services.GetRequiredService<MainWindow>();
+            Window.Show();
+        }
+
+    }
+
+}
